9c933d4b737fda8321d5f6fc3709ab434d110f08
ondra.koritak <ondra@trigael.com>
2025-06-02 15:48:37 +0200

    Fixed Logger import

--- src/logger/middlewares/trace-id.middleware.ts
+++ src/logger/middlewares/trace-id.middleware.ts
@@ -1,13 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { nanoid } from 'nanoid';
+
 import { AsyncLocalStorage } from 'async_hooks';
 
 export const traceStorage = new AsyncLocalStorage<Map<string, string>>();
 
 @Injectable()
 export class TraceIdMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { nanoid } = await import('nanoid');
     const store = new Map<string, string>();
     const traceId = nanoid(6); 
     const ip = req.ip ?? req.headers['x-forwarded-for'] ?? req.socket.remoteAddress;
