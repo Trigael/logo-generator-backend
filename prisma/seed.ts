aac21c1d9fa7165949ebb5b265f6dd447b42000c
ondra.koritak <ondra@trigael.com>
2025-07-07 13:17:41 +0200

    Added prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/**
+ * This file creates initial data for database
+ * Run: npx prisma db seed
+ */
+
+async function main() {
+  // Inserting data
+  await productTypesSeed();
+
+  console.log('[DB] Seed data inserted.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+
+async function productTypesSeed() {
+    console.log('[DB] Creating seed for product_types...');
+
+    const existing = await prisma.product_types.findFirst();
+  
+    // Checking if there is any data
+    if (existing) {
+      console.log('[DB] Seed for product_types already exists, skipping.');
+
+      return;
+    }
+
+    await prisma.product_types.create({
+        data: { name: 'generated_logo' }
+    })
+}
