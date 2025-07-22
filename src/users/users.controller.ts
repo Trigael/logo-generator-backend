875a6d3d2af2e9c40aefe8e5b04e18a0145d0f0d
ondra.koritak <ondra@trigael.com>
2025-07-22 10:53:22 +0200

    added new endpoint: /users/get/:email

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor(
+      private readonly usersService: UsersService,
+    ) {}
+    
+    @Get('get/:email')
+    getOrCreateGuestUser(@Param('email') email: string) {
+        return this.usersService.getOrCreateGuestUser(email)
+    }
+}
